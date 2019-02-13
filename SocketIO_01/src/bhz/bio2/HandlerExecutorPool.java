c3fd526f8de0101b4e361a8bcb4374ddf8a1bbbb
1047984367@qq.com <as1314520>
2019-02-13 15:31:42 +0800

    第一次提交

--- SocketIO_01/src/bhz/bio2/HandlerExecutorPool.java
+++ SocketIO_01/src/bhz/bio2/HandlerExecutorPool.java
@@ -8,6 +8,7 @@ import java.util.concurrent.TimeUnit;
 public class HandlerExecutorPool {
 
 	private ExecutorService executor;
+
 	public HandlerExecutorPool(int maxPoolSize, int queueSize){
 		this.executor = new ThreadPoolExecutor(
 				Runtime.getRuntime().availableProcessors(),
--- SocketIO_01/src/bhz/nio/Server.java
+++ SocketIO_01/src/bhz/nio/Server.java
@@ -3,6 +3,10 @@ package bhz.nio;
 import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.nio.ByteBuffer;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
 import java.util.Iterator;
 
 public class Server implements Runnable{
